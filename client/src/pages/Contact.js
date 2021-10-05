23acfc2e2f86e5f8547007590b76ada190e8b887
Jessica <jlroxi10@gmail.com>
2021-10-04 22:55:01 -0700

    contact is slightly acceptable

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -7,19 +7,54 @@ import facebook from '../img/social-icons/facebook.png';
 import gmail from '../img/social-icons/gmail.png';
 
 // react components
+import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import FormCheck from 'react-bootstrap/FormCheck';
+import FormControl from 'react-bootstrap/FormControl';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/esm/Container';
 
 function Contact() {
   return (
     <>
     <div className="Contact">
-      <header className="App-header">
+      <h2 className="mainText">Contact Us</h2>
         {/* modal section */}
+    <Container>
+    <Col id="modal">
+      <Form>
         <h4>Want to learn more?</h4>
         <p>We care about your input and experiences!</p>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Name"
+            className="mb-3"
+            >
+            <Form.Control type="name" placeholder="Your name" />
+          </FloatingLabel>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <FloatingLabel
+            controlId="floatingInput"
+            label="Email address"
+            className="mb-3"
+            >
+          <Form.Control type="email" placeholder="Enter email" />
+        </FloatingLabel>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicMessage">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows={5} placeholder="Type a message..." />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
         {/* add inputs here: name, email address, type a message */}
-      </header>
-
+      </Col>
       <Col>
       <h4 className="contactMethod">
                       <a href="https://www.gmail.com" target="_blank">
@@ -57,6 +92,7 @@ function Contact() {
                       </a>
                     </h4>
       </Col>
+      </Container>
     </div>
     </>
   );
