398778fc3f71fededa9d4a4ad3d1cf06b481ae1a
brett <b.woltz123@gmail.com>
2021-10-14 19:58:42 -0700

    recover App.css and index.css

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
