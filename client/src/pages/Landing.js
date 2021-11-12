8a0fb9c784b911ea5bd249ea30f957b959451e96
arai713 <astha.rai713@gmail.com>
2021-11-11 19:06:07 -0800

    Removed weird svg separator from banner

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -59,22 +59,7 @@ function Landing() {
                 </div>
               </Container>
               {/* SVG separator */}
-              <div className="separator separator-bottom separator-skew">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  preserveAspectRatio="none"
-                  version="1.1"
-                  viewBox="0 0 2560 100"
-                  x="0"
-                  y="0"
-                >
-                  <polygon
-                    className="fill-white"
-                    // style={{fill:"#FCFAD9"}}
-                    points="2560 0 2560 100 0 100"
-                  />
-                </svg>
-              </div>
+              
             </section>
           </div>
         </div>
