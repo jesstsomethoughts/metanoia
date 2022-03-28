4e992146c19de03c000d8c3c97be1e99a6747f6d
Mc Donald kasonga <mulumbamack@gmail.com>
2022-03-28 13:24:41 -0700

    Added style get to started bttn and catalog navbar

--- client/src/pages/Catalog.js
+++ client/src/pages/Catalog.js
@@ -1,35 +1,35 @@
-import '../assets/css/Catalog.css';
-import React from 'react';
+import '../assets/css/Catalog.css'
+import React from 'react'
 
 // React components
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 // images and other components
-import Banner from '../assets/img/website-image.png';
-import Placeholder from '../assets/img/metanoia_newlogo.png';
-import BookList from '../data/olBookList.json';
+import Banner from '../assets/img/website-image.png'
+import Placeholder from '../assets/img/metanoia_newlogo.png'
+import BookList from '../data/olBookList.json'
 
 const BookCard = ({ cover, title, author, url }) => {
   const styleImg = {
     width: '100%',
     height: '40vh',
     objectFit: 'cover',
-  };
+  }
 
   return (
-    <Col class="col-3">
-      <Card className="book-card" onClick={() => window.open(url, '_blank')}>
-        <Card.Img style={styleImg} variant="top" src={cover} />
+    <Col class='col-3'>
+      <Card className='book-card' onClick={() => window.open(url, '_blank')}>
+        <Card.Img style={styleImg} variant='top' src={cover} />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>By {author}</Card.Text>
         </Card.Body>
       </Card>
     </Col>
-  );
-};
+  )
+}
 
 function Catalog() {
   //<img src='http://covers.openlibrary.org/b/olid/" + BookList[0]["bookIds"][0] + "-M.jpg'>
@@ -39,9 +39,9 @@ function Catalog() {
   // console.log(BookList[1]["records"][`/books/${BookList[0]["bookIds"][0]}`].details.details.authors[0].name) //author name
 
   const loadBooks = (list) => {
-    let ar = [];
+    let ar = []
     for (let index = 0; index < list.length; index++) {
-      let i = list[index];
+      let i = list[index]
       ar.push(
         <BookCard
           cover={
@@ -62,55 +62,57 @@ function Catalog() {
               .details.preview_url
           }
         />
-      );
+      )
     }
-    return ar;
-  };
+    return ar
+  }
 
   return (
     <>
       {/* <div>
         <img alt="..." src={Banner} className="MainBanner" />
     </div> */}
-      <h1 className="header">Miscellaneous</h1>
-      <div className="section">
-        <h2 className="section-header">Grades K-2</h2>
-        <Row className="book-row" class="flew-row flex-nowrap">
+      <h1 className='header'>Miscellaneous</h1>
+      <div className='section'>
+        <h2 className='section-header'>Grades K-2</h2>
+        <Row className='book-row' class='flew-row flex-nowrap'>
           {loadBooks([0, 3, 4, 6])}
         </Row>
       </div>
-      <div className="section">
-        <h2 className="section-header">Grades 3-5</h2>
-        <Row className="book-row" class="flew-row flex-nowrap">
+      <div className='section'>
+        <h2 className='section-header'>Grades 3-5</h2>
+        <Row className='book-row' class='flew-row flex-nowrap'>
           {loadBooks([1, 2, 8, 9])}
         </Row>
       </div>
-      <div className="section">
-        <h2 className="section-header">Grades 6-8</h2>
-        <Row className="book-row" class="flew-row flex-nowrap">
+      <div className='section'>
+        <h2 className='section-header'>Grades 6-8</h2>
+        <Row className='book-row' class='flew-row flex-nowrap'>
           {loadBooks([5, 7])}
         </Row>
       </div>
 
-      <nav aria-label="...">
-        <ul class="pagination">
-          <li class="page-item">
-            <a class="page-link" href="/diversityCatalog">
+      <nav aria-label='...'>
+        <ul class='pagination'>
+          <li class='page-item'>
+            <a class='page-link the-diversity' href='/diversityCatalog'>
               Diversity
             </a>
           </li>
-          <li class="page-item active">
-            <span class="page-link">Miscellaneous</span>
+          <li class='page-item active  '>
+            <a class='page-link the-miscell' href='/miscellanousCatalog'>
+              Miscellaneous
+            </a>
           </li>
-          <li class="page-item">
-            <a class="page-link" href="/friendshipCatalog">
+          <li class='page-item'>
+            <a class='page-link the-friendship' href='/friendshipCatalog'>
               Friendship
             </a>
           </li>
         </ul>
       </nav>
     </>
-  );
+  )
 }
 
-export default Catalog;
+export default Catalog
