5f40a3bd6fa3d1ff5322eac8bd24a9ccf399f191
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2022-04-07 23:19:06 -0700

    Remove unused styling replaced by PageTitle component and styling

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,38 +1,38 @@
 import '../assets/css/About.css';
+import PageTitle from "../components/PageTitle";
 
 // additional images/files
-import logo from '../assets/img/metanoia_newlogo.png';
-import AsthaImage from '../assets/img/astha.JPG';
-import JessicaImage from '../assets/img/jessica.png';
-import TanveeImage from '../assets/img/tanvee.png';
-import CassidyImage from '../assets/img/cassidy.png';
-import BrettImage from '../assets/img/brett.png';
-import JoyceImage from '../assets/img/joyce.jpg';
+import logo from "../assets/img/metanoia_newlogo.png";
+import AsthaImage from "../assets/img/astha.JPG";
+import JessicaImage from "../assets/img/jessica.png";
+import TanveeImage from "../assets/img/tanvee.png";
+import CassidyImage from "../assets/img/cassidy.png";
+import BrettImage from "../assets/img/brett.png";
+import JoyceImage from "../assets/img/joyce.jpg";
 
 // react components
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 function About() {
   return (
     <>
-    <div className="About">
-      <div id = "about-background">
-        <h2 id = "header_text">About Us</h2>
-      </div>
-      <div className="about-para">
-        <p>
-          Metanoia is an upcoming EdTech startup that seeks to put
-          social issues at the forefront of children’s lives
-          through our virtual interactive library. Our website
-          bridges the communication gap between parents/teachers
-          and students by tailoring personalized book
-          recommendations that foster meaningful discussions. K-5
-          students will be able to enrich their knowledge whenever
-          and wherever they go!
-        </p>
-      </div>
+      <div className="About">
+        <PageTitle titleText={"About Us"} />
+        <div className="about-para">
+          <p>
+            Metanoia is an upcoming EdTech startup that
+            seeks to put social issues at the forefront of
+            children’s lives through our virtual interactive
+            library. Our website bridges the communication
+            gap between parents/teachers and students by
+            tailoring personalized book recommendations that
+            foster meaningful discussions. K-5 students will
+            be able to enrich their knowledge whenever and
+            wherever they go!
+          </p>
+        </div>
 
         {/* Team Intros */}
         <Container className="team-pictures">
@@ -43,17 +43,26 @@ function About() {
           </Row>
           <Row className="first-row">
             <Col sm>
-              <img src={AsthaImage} className="team-image" />
+              <img
+                src={AsthaImage}
+                className="team-image"
+              />
               <h5>Astha Rai</h5>
               <h6>Co-Founder</h6>
             </Col>
             <Col sm>
-              <img src={JessicaImage} className="team-image" />
+              <img
+                src={JessicaImage}
+                className="team-image"
+              />
               <h5>Jessica Li</h5>
               <h6>Co-Founder</h6>
             </Col>
             <Col sm>
-              <img src={TanveeImage} className="team-image" />
+              <img
+                src={TanveeImage}
+                className="team-image"
+              />
               <h5>Tanvee Joshi</h5>
               <h6>Co-Founder</h6>
             </Col>
@@ -61,17 +70,26 @@ function About() {
 
           <Row>
             <Col>
-              <img src={CassidyImage} className="team-image" />
+              <img
+                src={CassidyImage}
+                className="team-image"
+              />
               <h5>Cassidy Cheng</h5>
               <h6>Head of BD/Marketing</h6>
             </Col>
             <Col>
-              <img src={JoyceImage} className="team-image" />
+              <img
+                src={JoyceImage}
+                className="team-image"
+              />
               <h5>Joyce Mok</h5>
               <h6>Head of UI/UX</h6>
             </Col>
             <Col>
-              <img src={BrettImage} className="team-image" />
+              <img
+                src={BrettImage}
+                className="team-image"
+              />
               <h5>Brett Woltz</h5>
               <h6>Head of Software + Web Development</h6>
             </Col>
