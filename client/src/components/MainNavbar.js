a809aaa316dcb2c88cd1da9aefbd403db63a6cea
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2022-07-21 16:51:04 -0700

    Convert MainNav to functional component, display user email from redux store

--- client/src/components/MainNavbar.js
+++ client/src/components/MainNavbar.js
@@ -1,118 +1,103 @@
 import '../assets/css/Navbar.css';
-import Headroom from 'headroom.js';
 
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 import Logo from '../assets/img/header_logo.png';
 // // import smallerLogo from "assets/img/brand/metanoia_newlogo.png"
+import { useSelector } from "react-redux";
 
-class MainNavbar extends React.Component {
-  componentDidMount() {
-    let headroom = new Headroom(document.getElementById('navbar-main'));
-    // initialise
-    headroom.init();
-    window.addEventListener('resize', this.handleResize);
-  }
-  componentDidMount() {
-    window.addEventListener('resize', this.handleResize);
-  }
-  componentWillUnmount() {
-    window.addEventListener('resize', this.handleResize);
-  }
+function MainNavbar() {
+  const userData = useSelector((state) => state.userData);
 
-  handleResize = (e) => {
-    this.setState({ windowWidth: window.innerWidth });
-  };
-
-  state = {
-    collapseClasses: '',
-    collapseOpen: false,
-    windowWidth: window.innerWidth,
-  };
-
-  onExiting = () => {
-    this.setState({
-      collapseClasses: 'collapsing-out',
-    });
-  };
-
-  onExited = () => {
-    this.setState({
-      collapseClasses: '',
-    });
-  };
-
-  render() {
-    return (
-      <Navbar variant="dark" className="navbar-main" expand="lg" sticky="top">
-        <Container>
-          <Navbar.Brand className="mr-lg-5" to="/">
+  return (
+    <Navbar variant="dark" className="navbar-main" expand="lg" sticky="top">
+      <Container>
+        <Navbar.Brand className="mr-lg-5" to="/">
+          <Nav.Link
+            as={ Link }
+            className="nav-link mx-3"
+            activeClassName="nav-link-active"
+            to="/"
+          >
+            <img className="logo" style={{ height: '60px' }} alt="..." src={Logo} />
+          </Nav.Link>
+        </Navbar.Brand>
+        <Navbar.Toggle
+          className="nav-toggle"
+          aria-controls="basic-navbar-nav"
+        />
+        <Navbar.Collapse>
+          <Nav className="ms-auto ml-5">
             <Nav.Link
+              as={ Link }
               className="nav-link mx-3"
               activeClassName="nav-link-active"
-              href="/"
+              to="/"
             >
-              <img className="logo" style={{ height: '60px' }} alt="..." src={Logo} />
+              Home
             </Nav.Link>
-          </Navbar.Brand>
-          <Navbar.Toggle
-            className="nav-toggle"
-            aria-controls="basic-navbar-nav"
-          />
-          <Navbar.Collapse>
-            <Nav className="ms-auto ml-5">
-              <Nav.Link
-                className="nav-link mx-3"
-                activeClassName="nav-link-active"
-                href="/"
-              >
-                Home
-              </Nav.Link>
-              <Nav.Link
-                className="nav-link mx-3"
-                activeClassName="nav-link-active"
-                href="/blog"
-              >
-                Blog
-              </Nav.Link>
-              <Nav.Link
-                className="nav-link mx-3"
-                activeClassName="nav-link-active"
-                href="/friendshipCatalog"
-              >
-                Catalog
-              </Nav.Link>
-              <Nav.Link
-                className="nav-link mx-3"
-                activeClassName="nav-link-active"
-                href="/about"
-              >
-                About
-              </Nav.Link>
-              <Nav.Link
-                className="nav-link mx-3"
-                activeClassName="nav-link-active"
-                href="/contact"
-              >
-                Contact
-              </Nav.Link>
-            </Nav>
-            <Button
-              className="btn-light btn-icon mx-3"
-              href="/subscribe"
-              id="subscribe"
+            <Nav.Link
+              as={ Link }
+              className="nav-link mx-3"
+              activeClassName="nav-link-active"
+              to="/blog"
             >
-              SUBSCRIBE
-            </Button>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
+              Blog
+            </Nav.Link>
+            <Nav.Link
+              as={ Link }
+              className="nav-link mx-3"
+              activeClassName="nav-link-active"
+              to="/friendshipCatalog"
+            >
+              Catalog
+            </Nav.Link>
+            <Nav.Link
+              as={ Link }
+              className="nav-link mx-3"
+              activeClassName="nav-link-active"
+              to="/about"
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              as={ Link }
+              className="nav-link mx-3"
+              activeClassName="nav-link-active"
+              to="/contact"
+            >
+              Contact
+            </Nav.Link>
+            {userData.user ?
+            <Nav.Link
+              as={ Link }
+              className="nav-link mx-3"
+              activeClassName="nav-link-active"
+              to="/settings"
+            >
+              {userData.user.email}
+              </Nav.Link>
+              :
+              <></>
+            }
+          </Nav>
+          <Button
+            as={ Link }
+            className="btn-light btn-icon mx-3"
+            to="/subscribe"
+            id="subscribe"
+          >
+            SUBSCRIBE
+          </Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default MainNavbar;
