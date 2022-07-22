123e12852e7fec64dd27a8412d211045f3c1bc25
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2022-07-22 15:46:49 -0700

    Fix footer links to not reload, scroll to top

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,32 +1,37 @@
 import React from 'react'
 import '../assets/css/Footer.css';
+import Nav from 'react-bootstrap/Nav';
 import { SocialIcon } from 'react-social-icons';
 import { Link } from "react-router-dom";
 
 function Footer() {
-  return (
-    <div className='main_footer'>
-        <div className='footer_container'>
-            <div className='content_container'>
-                <div className='social_medias'>
-                    <SocialIcon id="mail" bgColor="#ffffff" target="_blank" url="mailto:metanoiabooks.co@gmail.com" />
-                    <SocialIcon id="linkedin" bgColor="#ffffff" target="_blank" url="https://www.linkedin.com/company/metanoiabooks/" />
-                    <SocialIcon id="facebook" bgColor="#ffffff" target="_blank" url="https://www.facebook.com/MetanoiaLibrary/"/>
-                </div>
-                <div className="footer_links">
-                    <Link className="indiv_links" target="_top" to="/"> Home</Link>
-                    <Link className="indiv_links" target="_top" to="/about"> About</Link>
-                    <Link className="indiv_links" target="_top" to="/blog"> Blog</Link>
-                    <Link className="indiv_links" target="_top" to="/contact"> Contact</Link>
-                    <Link className="indiv_links" target="_top" to="/subscribe"> Subscribe</Link>
-                </div>
-                <div className="comp_name">
-                &copy; 2022 Metanoia Books Co.
+    function scrollToTop() {
+        window.scrollTo(0, 0);
+    }
+
+    return (
+        <div className='main_footer'>
+            <div className='footer_container'>
+                <div className='content_container'>
+                    <div className='social_medias'>
+                        <SocialIcon id="mail" bgColor="#ffffff" target="_blank" url="mailto:metanoiabooks.co@gmail.com" />
+                        <SocialIcon id="linkedin" bgColor="#ffffff" target="_blank" url="https://www.linkedin.com/company/metanoiabooks/" />
+                        <SocialIcon id="facebook" bgColor="#ffffff" target="_blank" url="https://www.facebook.com/MetanoiaLibrary/"/>
+                    </div>
+                    <div className="footer_links">
+                        <Nav.Link onClick={scrollToTop} as={ Link } className="indiv_links" to="/">Home</Nav.Link>
+                        <Nav.Link onClick={scrollToTop} as={ Link } className="indiv_links" to="/about">About</Nav.Link>
+                        <Nav.Link onClick={scrollToTop} as={ Link } className="indiv_links" to="/blog">Blog</Nav.Link>
+                        <Nav.Link onClick={scrollToTop} as={ Link } className="indiv_links" to="/contact">Contact</Nav.Link>
+                        <Nav.Link onClick={scrollToTop} as={ Link } className="indiv_links" to="/subscribe">Subscribe</Nav.Link>
+                    </div>
+                    <div className="comp_name">
+                    &copy; 2022 Metanoia Books Co.
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Footer
