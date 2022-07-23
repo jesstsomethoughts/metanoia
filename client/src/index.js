377ffdd641a1671f135bc5e76f9ae4ba4b680f72
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2022-07-22 18:01:41 -0700

    Add persisting state

--- client/src/index.js
+++ client/src/index.js
@@ -11,12 +11,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { store } from './store/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userSlice from '../state/userSlice';
+
+const persistConfig = {
+  key: 'user',
+  version: 1,
+  storage,
+}
+
+const persistedUserReducer = persistReducer(persistConfig, userSlice);
 
 export const store = configureStore({
   reducer: {
-    userData: userSlice,
+    userData: persistedUserReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
