7d4a30109db03c90a53123a3c9a42bb2dd58ce33
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2022-08-26 15:53:07 -0700

    Dispatch thunk for creating blog post

--- client/src/pages/BlogCreate.js
+++ client/src/pages/BlogCreate.js
@@ -3,55 +3,57 @@ import React, { useState } from "react";
 import PageTitle from "../components/PageTitle";
 import { Button, Form } from "react-bootstrap";
 import { Link } from 'react-router-dom';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { newBlogPost } from "../state/thunks/newBlogPost";
 
 import ReactMde from "react-mde";
 import ReactMarkdown from "react-markdown";
 import "react-mde/lib/styles/css/react-mde-all.css";
 
 function BlogCreate() {
+  const dispatch = useDispatch();
   const userData = useSelector((state) => state.userData);
-  const [postTitle, setPostTitle] = useState("");
-  const [value, setValue] = React.useState("");
+  const [postBody, setPostBody] = useState("");
+  const [postTitle, setPostTitle] = React.useState("");
   const [selectedTab, setSelectedTab] = React.useState("write");
 
-  // async function submitPost(e) {
-  //   e.preventDefault();
-  //   try {
-  //     // TODO use redux thunk to send to store and firebase.
-  //   } catch (error) {
-  //     console.log("Error: ", error);
-  //   }
-  // }
+  async function submitPost(e) {
+    e.preventDefault();
+    try {
+      dispatch(newBlogPost({
+        title: postTitle,
+        body: postBody,
+        author: userData["user"]["providerData"][0]["displayName"] || "Metanoia",
+        description: ""
+      }))
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  }
 
-  // function updatePostInfo(e) {
-  //   const name = e.target.name;
-  //   let value = e.target.value;
-  //   setPostInfo({
-  //     ...postInfo,
-  //     [name]: value,
-  //   });
-  // }
+  function updatePostTitle(e) {
+    setPostTitle(e.target.value);
+  }
 
   return (
     <>
       <div className="form-page">
         <PageTitle titleText={"Create New Blog Post"} />
         {userData && userData.user && userData.user.uid ?
-          <Form className="form-blog">
+          <Form className="form-blog" onSubmit={submitPost}>
             <Form.Group>
               <Form.Label>Post Title</Form.Label>
               <Form.Control
                 name="title"
                 type="text"
                 placeholder="Enter title"
-                onChange={setPostTitle}
+                onChange={updatePostTitle}
               />
             </Form.Group>
             <div className="markdown-container">
               <ReactMde
-                value={value}
-                onChange={setValue}
+                value={postBody}
+                onChange={setPostBody}
                 selectedTab={selectedTab}
                 onTabChange={setSelectedTab}
                 generateMarkdownPreview={markdown =>
@@ -59,7 +61,7 @@ function BlogCreate() {
                 }
               />
             </div>
-            <Button>Submit Post</Button>
+            <Button variant="primary" type="submit">Submit Post</Button>
           </Form>
           :
           <div className="need-login-message">Please <Link to="/signin">sign in</Link> to create a blog post.</div>
