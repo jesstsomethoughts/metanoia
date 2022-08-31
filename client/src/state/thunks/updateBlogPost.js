121044da28a42a0d3bea3b1d71188f8ddfa165fc
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2022-08-30 17:11:24 -0700

    Add image

--- client/src/state/thunks/updateBlogPost.js
+++ client/src/state/thunks/updateBlogPost.js
@@ -3,14 +3,15 @@ import { db } from '../../firebase.js';
 import { doc, updateDoc } from "firebase/firestore";
 
 export const updateBlogPost = createAsyncThunk('blogData/updateBlogPost', async (postObj) => {
-  const {title, author, description, body, id} = postObj;
+  const {title, author, description, image, body, id} = postObj;
 
   try {
     const ref = await updateDoc(doc(db, "blogs-markdown", id), {
       title,
       author,
       description,
-      body,
+      image,
+      body
     });
 
     return {title, author, description, body};
